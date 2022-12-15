afd963cbd45af9c4435721d018c75f87eb572cd5
Ruyago <ruyago@gmail.com>
2022-12-15 17:09:35 +0100

    fpöder deöeted

--- config/index.js
+++ config/index.js
@@ -19,9 +19,8 @@ module.exports = (app) => {
 
   app.use(
     cors({
-      origin: ["http://localhost:3000"],
-    })
-  );
+      origin: process.env.FRONTEND_URL ||  'http://localhost:3000'   })
+  )
 
   // In development environment the app logs
   app.use(logger("dev"));
