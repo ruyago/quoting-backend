aa3cc5d17fadbcc9f2a12c59f874c873e8e2a1e3
Ruyago <ruyago@gmail.com>
2022-12-15 19:44:07 +0100

    cors changed

--- config/index.js
+++ config/index.js
@@ -19,7 +19,7 @@ module.exports = (app) => {
 
   app.use(
     cors({
-      origin: process.env.FRONTEND_URL ||  'http://localhost:3000',
+      origin: process.env.FRONTEND_URL,
       credentials: true,   
     })
   )
