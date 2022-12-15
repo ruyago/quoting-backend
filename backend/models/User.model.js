706b82e2b1a7f2018cb56262e1414f0c8d6f20eb
Ruyago <ruyago@gmail.com>
2022-12-15 11:38:05 +0100

    only 1 Like per User

--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -5,7 +5,8 @@ const userSchema = new Schema({
   email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
-  favQuotes: [{ type: Schema.Types.ObjectId,  ref: "Quote"}]
+  favQuotes: [{ type: Schema.Types.ObjectId,  ref: "Quote"}],
+  userLikes: [{ type: Schema.Types.ObjectId,  ref: "Quote"}],
 });
 
 module.exports = model("User", userSchema);
--- backend/routes/likes.routes.js
+++ backend/routes/likes.routes.js
@@ -3,17 +3,24 @@ const router = express.Router();
 const mongoose = require("mongoose");
 
 const Quote = require("../models/quote.model");
+const User= require("../models/User.model");
 
 router.put("/my-quotes/add-like/:quoteId", (req, res, next) => {
     const { quoteId } = req.params;
-  
+    const {userId} = req.body;
+
+    console.log(userId)
     if (!mongoose.Types.ObjectId.isValid(quoteId)) {
       res.status(400).json({ message: "Specified id is not valid" });
       return;
     }
   
-    Quote.findByIdAndUpdate(quoteId, {$inc : {likes: 1}}, { new: true })
-      .then((updatedQuote) => res.json(updatedQuote))
+    return Quote.findByIdAndUpdate(quoteId, {$inc : {likes: 1}}, { new: true })
+    .then ((resp)=>{
+    return User.findByIdAndUpdate(userId, {$push : {userLikes: quoteId}}, { new: true })})
+      .then((updatedQuote) => 
+      {console.log("hello backend", updatedQuote);
+      res.json(updatedQuote)})
       .catch((error) => res.json(error));
   });
 
